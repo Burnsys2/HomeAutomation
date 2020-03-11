3200ed629105af351010574d60196c7b195a0d84
Pablo Crisolini <pablo@alephcrm.com>
2020-03-10 22:48:20 -0300

    cambios

--- Arduino/Utilidades/RFEmitter/GrabarRemotos/GrabarRemotos.ino
+++ Arduino/Utilidades/RFEmitter/GrabarRemotos/GrabarRemotos.ino
@@ -15,7 +15,7 @@ void setup() {
   
   // Transmitter is connected to Arduino Pin #10  
   Serial.print("test");
-  mySwitch.enableTransmit(3);
+  mySwitch.enableTransmit(18);
 
   // Optional set pulse length.
    mySwitch.setPulseLength(320);
@@ -32,15 +32,17 @@ void loop() {
   /* Same switch as above, but using binary code */
  // mySwitch.send("1011011100011011000001"); //Control 1 A
  // mySwitch.send("1011011100011011000010");  //Control 1 B
-  mySwitch.send("100000101100100011010011");  //Control 1 C
+  mySwitch.send("100000101100100011001000");  //Control 1 C
   //mySwitch.send("1011011100011011001010"); //Control 1 D
   Serial.print("SEND");
+    delay(3000);  
+
  // mySwitch.send("1011011100011011001011"); //Control 2 A
  // mySwitch.send("1011011100011011001100");  //Control 2 B
   //mySwitch.send("1011011100011011001101");  //Control 2 C
-//mySwitch.send("1011011100011011001111"); //Control 2 D
+  mySwitch.send("10011000100101101000001"); //Control 2 D
   
-  delay(1000);  
+  delay(3000);  
 
 
 }
